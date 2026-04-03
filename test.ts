15dc994c8f776a1bf61c0ac114ee3b35f0112461
Shambu T N <shambutn@gmail.com>
2026-04-03 09:55:11 +0530

    1st commit

---
+++ test.ts
@@ -0,0 +1,16 @@
+function isPrime(num: number): boolean {
+  if (num <= 1) return false;
+  if (num <= 3) return true;
+  if (num % 2 === 0 || num % 3 === 0) return false;
+
+  for (let i = 5; i * i <= num; i += 6) {
+    if (num % i === 0 || num % (i + 2) === 0) return false;
+  }
+  return true;
+}
+
+for (let i = 1; i <= 17; i++) {
+  if (isPrime(i)) {
+    console.log(i);
+  }
+}
